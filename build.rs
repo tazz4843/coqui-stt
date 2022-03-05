faeaadbc465607c8260edf21dac483c67f66772b
0/0 <zero@imaskeleton.me>
2022-03-05 12:09:40 -0700

    allow building on Windows: issue #3

---
+++ build.rs
@@ -0,0 +1,14 @@
+// required thingies for building this lib on Windows platforms
+// see issue 3 for more info
+// https://github.com/tazz4843/coqui-stt/issues/3
+
+#[cfg(target_os = "windows")]
+fn main() {
+    // necessary libraries: libstt.so, libkenlm.so, libstt.so.if.lib and libkenlm.so.if.lib.
+    println!(r"cargo:rustc-link-search=C:\stt");
+    println!("cargo:rustc-link-lib=dylib=libstt.so.if");
+    println!("cargo:rustc-link-lib=dylib=libkenlm.so.if");
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {}
